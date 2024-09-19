21fde1639e067b69fbcba17ae93fcfbfdedeacc2
Ingage-PatrickSchirmer <patrick.schirmer@ingagepartners.com>
2024-09-19 10:33:31 -0400

    sharepoint repo init

---
+++ rc-theme.ps1
@@ -0,0 +1,27 @@
+# revision-theme.ps1
+$theme = @{
+    themePrimary = "#B73584";
+    themeLighterAlt = "#fcf5f9";
+    themeLighter = "#f4d8e9";
+    themeLight = "#eab8d6";
+    themeTertiary = "#d47ab0";
+    themeSecondary = "#c04890";
+    themeDarkAlt = "#a53076";
+    themeDark = "#8c2864";
+    themeDarker = "#671e4a";
+    neutralLighterAlt = "#0d1232";
+    neutralLighter = "#0d1231";
+    neutralLight = "#0d112f";
+    neutralQuaternaryAlt = "#0c102c";
+    neutralQuaternary = "#0b0f2a";
+    neutralTertiaryAlt = "#0b0f28";
+    neutralTertiary = "#c8c8c8";
+    neutralSecondary = "#d0d0d0";
+    neutralSecondaryAlt = "#d0d0d0";
+    neutralPrimaryAlt = "#dadada",
+    neutralPrimary = "#fff";
+    neutralDark = "#f4f4f4";
+    black = "#f8f8f8";
+    white = "#0E1333";
+  
+}
